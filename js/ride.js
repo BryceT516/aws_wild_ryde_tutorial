1231b7150ff4a1349b26ac919adc89cf287d5e73
Bryce Tucker <bryce.tucker@gmail.com>
2020-02-02 23:09:17 +0000

    switching back to other lambda.

--- js/ride.js
+++ js/ride.js
@@ -18,7 +18,7 @@ WildRydes.map = WildRydes.map || {};
     function requestUnicorn(pickupLocation) {
         $.ajax({
             method: 'POST',
-            url: _config.api.invokeUrl + '/ruby-ride',
+            url: _config.api.invokeUrl + '/ride',
             headers: {
                 Authorization: authToken
             },
